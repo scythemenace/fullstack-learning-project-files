48beb9a262d42e934adbcc6f1b35f53604fda961
Ankur Pandey <pankur216@gmail.com>
2024-06-30 19:23:39 -0400

    Completed palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,30 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase();
+  str = str.split(" ").join("");
+  str = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+  if (str.length == 0) {
+    return true;
+  }
+  i = 0;
+  j = str.length - 1;
+
+  while (i < j) {
+    //console.log(`Curr value of i: ${i}`);
+    //console.log(`Curr value of j: ${j}`);
+    if (str[i] != str[j]) {
+      //console.log(`${str[i]} is not equal to ${str[j]}`);
+      return false;
+    }
+
+    i++;
+    j--;
+  }
+
   return true;
 }
+
+//console.log(isPalindrome('Able, was I ere I saw Elba!'));
 
 module.exports = isPalindrome;
