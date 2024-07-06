eccfe9287f05697112c93b490eca8c8d6c1b76b4
Ankur Pandey <pankur216@gmail.com>
2024-07-05 22:22:05 -0400

    Did the third hard q without returning Promise.all, just returning a Promise

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,44 @@
  */
 
 function wait1(t) {
-
+    return new Promise ( (resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        },t*1000);
+    })
 }
 
 function wait2(t) {
-
+    return new Promise ( (resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        },t*1000);
+    })
 }
 
 function wait3(t) {
+    return new Promise ( (resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        },t*1000);
+    })
 
 }
 
-function calculateTime(t1, t2, t3) {
+async function calculateTime(t1, t2, t3) {
+    currentTime = Date.now();
+    
+    await Promise.all([wait1(t1), wait2(t2), wait3(t3)]);
 
+    updatedTime = Date.now();
+
+    timeTaken = updatedTime - currentTime;
+
+    return new Promise ( (resolve, reject) => {
+        resolve(timeTaken);
+    })
 }
+
+
 
 module.exports = calculateTime;
