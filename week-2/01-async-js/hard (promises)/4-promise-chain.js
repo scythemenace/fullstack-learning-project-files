a7cbd220e044b68504ecbf0c43a429aba70ee0aa
Ankur Pandey <pankur216@gmail.com>
2024-07-06 04:08:50 -0400

    Completed all hard questions

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -5,20 +5,45 @@
  * Compare it with the results from 3-promise-all.js
  */
 
-function wait1(t) {
-
-}
+    function wait1(t) {
+        return new Promise( (resolve, reject) => {
+            setTimeout(() => {
+                resolve();
+            }, t*1000);
+        })
+    }
 
 function wait2(t) {
+    return new Promise ( (resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, t*1000);
+    })
 
 }
 
 function wait3(t) {
-
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, t*1000);
+    })
 }
 
 async function calculateTime(t1, t2, t3) {
+    return new Promise( (resolve, reject) => {
+        let startTime = Date.now();
+        resolve(startTime);
+    }).then( async (startTime) => {
+        result1 = await wait1(t1);
+        result2 = await wait2(t2);
+        result3 = await wait3(t3);
+        const endTime = Date.now();
+        let timeTaken = endTime - startTime;
+        return timeTaken;
+    })
 
 }
+
 
 module.exports = calculateTime;
